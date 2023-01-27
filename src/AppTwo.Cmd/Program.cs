e2a1b6365643b194f77486f39878e6be97c53d99
ronh-iq <ron.hodgson@iqcu.com>
2023-01-26 16:24:24 -0800

    add project references and main

--- src/AppTwo.Cmd/Program.cs
+++ src/AppTwo.Cmd/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace AppTwo.Cmd
+﻿using LibAlpha.PrjOne;
+using LibAlpha.PrjTwo;
+using LibBeta.PrjOne;
+using LibBeta.PrjTwo;
+
+namespace AppTwo.Cmd
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var libAlphaPrjOneClass = new LibAlphaPrjOneClass();
+            var libAlphaPrjTwoClass = new LibAlphaPrjTwoClass();
+            var libBetaPrjOneClass = new LibBetaPrjOneClass();
+            var libBetaPrjTwoClass = new LibBetaPrjTwoClass();
+
+            Console.WriteLine(
+                libBetaPrjTwoClass.AppendName(
+                    libBetaPrjOneClass.AppendName(
+                        libAlphaPrjTwoClass.AppendName(
+                            libAlphaPrjOneClass.AppendName("Testing:")))));
         }
     }
 }
